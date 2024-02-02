c593e2197fb15da3e67a36dd16488ac0067e67b4
대철 김 <artie@langcode.io>
2024-02-02 19:29:57 +0900

    접속 오류가 함수를 호출해야 출력되도록

--- service/cosmos.py
+++ service/cosmos.py
@@ -7,13 +7,13 @@ endpoint = "https://networktestcosmos.documents.azure.com:443/"
 key = "YIVMHcMc8TpTE1N9AWtPrAorviKHIcYmG3eFBBSOK8aNKcCn6TQ8fQXFAj5G564GavN06H1yH0u4ACDbjB7nKg=="
 
 # Cosmos DB 클라이언트 생성
-client = CosmosClient(endpoint, key)
 
 # 데이터베이스 및 컨테이너 정보
 database_name = "main"
 
 
 def getCosmosList(container_name):
+    client = CosmosClient(endpoint, key)
     database = client.get_database_client(database_name)
     container = database.get_container_client(container_name)
     query = "Select * from c"
@@ -21,6 +21,7 @@ def getCosmosList(container_name):
     return items
 
 def writeCosmos(container_name,writeData):
+    client = CosmosClient(endpoint, key)
     database = client.get_database_client(database_name)
     writeData['pk'] = str(uuid.uuid4())
     writeData['id'] = writeData['pk']
