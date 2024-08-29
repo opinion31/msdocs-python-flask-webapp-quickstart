f23028870a6bf51ac4daf4c4c2a832c97a270730
Artie <opinion31@gmail.com>
2024-08-29 15:03:26 +0900

    키 수정함

--- service/cosmos.py
+++ service/cosmos.py
@@ -10,27 +10,16 @@ endpoint = "https://networktestcosmos.documents.azure.com:443/"
 key = "YIVMHcMc8TpTE1N9AWtPrAorviKHIcYmG3eFBBSOK8aNKcCn6TQ8fQXFAj5G564GavN06H1yH0u4ACDbjB7nKg=="
 key_vault_url = "https://artiekeyvault.vault.azure.net/"
 
-
-# Cosmos DB 클라이언트 생성
-
-# DefaultAzureCredential을 사용하여 인증
-
-# SecretClient 초기화
-client = SecretClient(vault_url=key_vault_url, credential=credential)
-
 # 비밀 가져오기
 secret_name = "cosmosendpoint"
-cosmos_key = client.get_secret(secret_name)
-
-# 비밀 출력
-print(f"Retrieved secret value: {retrieved_secret.value}")
-
 
 # 데이터베이스 및 컨테이너 정보
 database_name = "main"
-
+keyvault_client = SecretClient(vault_url=key_vault_url, credential=credential)
+cosmos_key = keyvault_client.get_secret(secret_name).value
 
 def getCosmosList(container_name):
+    print(cosmos_key)
     client = CosmosClient(endpoint, cosmos_key)
     database = client.get_database_client(database_name)
     container = database.get_container_client(container_name)
